466f61b799fbf22985938213ff03a8883d6c56e6
Mayara (Code.maypy) <code.maypy@gmail.com>
2026-01-22 11:41:50 -0300

    add comentarios

--- Verificador_senha.py
+++ Verificador_senha.py
@@ -4,16 +4,25 @@ print("Vamos verificar se você tem uma senha boa...")
 senha = input("Digite a sua senha:")
 
 print("Analisando...")
+#time.sleep() faz uma pausa.
 time.sleep(2)
 
 simbolos ="!@#$%^&*()-_=+[{]};:'\",<.>/?|\\"
 
+# o any() verifica se ao menos uma condição pedida foi encontrada
 tem_simbolo = any(letra in simbolos for letra in senha)
 tem_maiuscula = any(letra.isupper() for letra in senha)
 tem_minuscula = any(letra.islower() for letra in senha)
 tem_numero = any(letra.isdigit() for letra in senha)
 
 
+
+'''
+len() -> ele conta a quantia de caracteres
+if -> verifica se a condição esta sendo atendida
+and -> adiciona mais "filtros" ao if.
+else -> se o if falho o else assume. 
+ '''
 
 
 if len(senha) >= 8 and tem_simbolo and tem_maiuscula and tem_minuscula and tem_numero:
